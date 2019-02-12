d684841ac9b306d03a09a58b383c05fc52547ecb
Beniamin Bia <biabeniamin@gmail.com>
2019-02-12 18:14:00 +0200

    project created

---
+++ Embedder.cpp
@@ -0,0 +1,39 @@
+#include "stdafx.h"
+#include "Embedder.h"
+
+using namespace cv;
+
+Embedder::Embedder()
+{
+}
+
+
+Embedder::~Embedder()
+{
+}
+
+bool Embedder::Embed(Mat input,
+	char* data,
+	int dataLenght,
+	Mat output) {
+
+	return true;
+}
+
+int Embedder::CountPixels(cv::Mat input)
+{
+	int pixels;
+
+	pixels = 0;
+
+	for (int i = 0; i < input.rows; i++)
+	{
+		for (int j = 0; j < input.cols; j++)
+		{
+			if (input.data[i * input.rows + j] > 0) 
+				pixels++;
+		}
+	}
+
+	return pixels;
+}
---
+++ Embedder.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <opencv2\opencv.hpp>
+
+class Embedder
+{
+public:
+	Embedder();
+	~Embedder();
+
+	bool Embed(cv::Mat input,
+		char* data,
+		int dataLenght,
+		cv::Mat output);
+
+	int CountPixels(cv::Mat input);
+};
+
